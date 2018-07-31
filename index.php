bfffb598f1949865734c9f33c644af3dfe32dfbf
rubycouzcedric <cousin.ruby.cedric@gmail.com>
2018-07-31 11:47:24 +0200

    ajout de css

--- index.php
+++ index.php
@@ -1,16 +1,26 @@
 <!DOCTYPE html>
 <html lang="fr" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <title>Exercice 1</title>
-  </head>
-  <body>
-    <p>
-Votre User Agent : <?= print_r($_SERVER['HTTP_USER_AGENT']); ?> <br />
-Votre adresse IP : <?= print_r($_SERVER['SERVER_ADDR']); ?><br >
+<head>
+  <meta charset="utf-8" />
+  <title>Exercice 1</title>
+  <link rel="stylesheet" href="assets/css/materialize.min.css">
+  <link rel="stylesheet" href="assets/css/style.css">
 
-Le nom du seveur : <?= print_r($_SERVER['SERVER_NAME']); ?>
+</head>
+<body>
+  <a class="waves-effect waves-light btn modal-trigger" href="#info">Vos information</a>
+  <div id="info" class="modal">
+    <div class="modal-content">
+      <h4>Vos informations</h4>
 
-    </p>
-  </body>
+      <p>Votre User Agent : <?= print_r($_SERVER['HTTP_USER_AGENT']); ?> </p> <!-- récupère l'user agent -->
+      <p>Votre adresse IP : <?= print_r($_SERVER['SERVER_ADDR']); ?></p><!-- récupère l'ip de l'utilisateur -->
+
+      <p>Le nom du seveur : <?= print_r($_SERVER['SERVER_NAME']); ?></p> <!-- récupère le nom du serveur -->
+    </div>
+  </div>
+  <script src="assets/js/jquery-3.3.1.min.js"></script>
+  <script src="assets/js/materialize.min.js"></script>
+  <script src="assets/js/script.js"></script>
+</body>
 </html>
